1251f30d83a34f81baffa86d1a934ef7f7e50a1e
Stefan Gojan <vollekannehoschi@gmail.com>
2014-01-02 16:42:16 +0100

    get recipes from db

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./source.sqlite');
 
+db.serialize(function() {
+	// get all user recipes
+	db.each("SELECT * FROM staging WHERE member_id=0", function(err, row) {
+		console.log(row.title);
+	});
+});
+
+db.close();
